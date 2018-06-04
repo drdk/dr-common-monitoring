drdk/dr-common-monitoring

commit 56348e844a1715684bd9fdd951e4ba4a88a979d5

TP-928 Added IHealthCheckExtra interface and logic for handling detailed Healthcheck information

src/DR.Common.Monitoring/Models/CommonHealthCheck.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DR.Common.Monitoring.Contract;
 
 namespace DR.Common.Monitoring.Models
@@ -24,11 +25,20 @@ namespace DR.Common.Monitoring.Models
                 bool? passed = null;
                 Exception exception = null;
                 string message = null;
+                IEnumerable<dynamic> details = null;
                 Status result;
                 Stopwatch.Restart();
                 try
                 {
-                    passed = RunTest(ref message, isPrivileged);
+                    var extras = this as IHealthCheckExtra;
+                    if (extras!=null)
+                    {
+                        extras.RunTestWithDetails(ref message, ref details, isPrivileged);
+                    }
+                    else
+                    {
+                        RunTest(ref message, isPrivileged);
+                    }
                 }
                 catch (Exception e)
                 {
@@ -41,6 +51,7 @@ namespace DR.Common.Monitoring.Models
                 {
                     Stopwatch.Stop();
                     result = new Status(passed: passed, duration: Stopwatch.Elapsed, message: message, exception: isPrivileged ? exception : null);
+                    result.Details = details;
                 }
                 return result;
             }
@@ -58,5 +69,6 @@ namespace DR.Common.Monitoring.Models
         /// </summary>
         /// <returns>True of success and False for failure. Should throw exceptions if possible.</returns>
         public abstract bool? RunTest(ref string message, bool isPrivileged = false);
+
     }
 }

src/DR.Common.Monitoring/Contract/IHealthCheckExtra.cs
@@ -1,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DR.Common.Monitoring.Contract
+{
+    public interface IHealthCheckExtra
+    {
+        bool? RunTestWithDetails(ref string message, ref IEnumerable<dynamic> details, bool isPrivileged = false);
+
+    }
+}

src/DR.Common.Monitoring/Models/Status.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DR.Common.Monitoring.Models
 {
@@ -41,5 +42,7 @@ namespace DR.Common.Monitoring.Models
             Message = message;
             Exception = exception;
         }
+
+        public IEnumerable<dynamic> Details { get; set; }
     }
 }

commit 7eddecbe95db4e007597f1584698f9f50361a681

Fixed critical bug introduced in 56348e844a1715684bd9fdd951e4ba4a88a979d5 , which made all check passed = null.

src/DR.Common.Monitoring/Models/CommonHealthCheck.cs
@@ -41,7 +41,7 @@ namespace DR.Common.Monitoring.Models
                 {
                     if (this is IHealthCheckExtra extras)
                     {
-                        extras.RunTestWithDetails(ref message, ref details, isPrivileged);
+                        passed = extras.RunTestWithDetails(ref message, ref details, isPrivileged);
                     }
                     else if (this is IExtendedHealthCheck extended)
                     {
@@ -49,10 +49,11 @@ namespace DR.Common.Monitoring.Models
                         message = testResult.Message;
                         details = testResult.Details;
                         reactions = testResult.Reactions;
+                        passed = testResult.Success;
                     }
                     else
                     {
-                        RunTest(ref message, isPrivileged);
+                        passed = RunTest(ref message, isPrivileged);
                     }
                 }
                 catch (Exception e)